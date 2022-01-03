3aba7c2f69ed27ef5255b5caea0b0b0b129c3935
Ali Barış Öztürk <alibarisozturk@gmail.com>
2022-01-03 15:00:50 +0300

    element plus

--- js/Main.vue
+++ js/Main.vue
@@ -1,12 +1,24 @@
 <template>
   <div>
     <button>Selam</button>
+    <el-button type="success" :icon="Star"></el-button>
   </div>
   
 </template>
+<style>
+@import "element-plus/theme-chalk/index.css";
+</style>
 <script>
+import {
+  Star
+} from '@element-plus/icons-vue'
 export default {
   name: 'Main',
+  data() {
+    return {
+      "Star":Star
+    }
+  },
   created() {
     console.log(document.cookie);
   }
