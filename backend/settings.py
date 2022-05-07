2530336eca030af53fb00ba727d0c818fa66f984
Ali Barış Öztürk <haan78@gmail.com>
2022-05-07 23:54:33 +0300

    logger

--- backend/settings.py
+++ backend/settings.py
@@ -1,4 +1,18 @@
 from lib.ENV import ENV
+import logging
+import time
+import os
+
+def applogger(name:str)->logging.Logger:
+    logfolder:str = "/var/log/app"
+    if not os.path.exists(logfolder):
+        os.makedirs(logfolder)
+    fh = logging.FileHandler(filename="{}/{}-{}.log".format(logfolder,name,time.strftime("%y%m%d%H%M%S")),mode="w")
+    fh.setFormatter(logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s"))
+    logger = logging.getLogger(name=name)
+    logger.addHandler(fh)
+    return logger
+
 class Settings:
     DBCONN: str
     DBNAME: str
--- test.py
+++ test.py
@@ -1,26 +1,3 @@
+import time
 
-from datetime import datetime
-from dateutil import parser
-import os
-from typing import Any, Type
-
-def ENVVal(name:str,type:Type,requierd:bool = True,default=None)->Any:
-    v = os.getenv(name)
-    if v is None:
-        if requierd == True:
-            raise Exception("ENV variable is required")
-        else:
-            return default
-    elif type is int:
-        return int(v)
-    elif type is float:
-        return float(v)
-    elif type is bool:
-        return bool(v)
-    elif type is str:
-        return str(v)
-    elif type is datetime:
-        return parser(v)
-    else:
-        raise Exception("Unsupported ENV type")
-
+print( time.strftime("%y%m%d%H%M%S") )
