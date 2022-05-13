525d7fa0e87ccd6c7c02c608b27df6c861cdae20
Ali Barış Öztürk <alibarisozturk@gmail.com>
2022-05-13 17:42:06 +0300

    hub.sh

--- hub.sh
+++ hub.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 
 #gerekli tanimlamalar
-imagename="projeadi"
+imagename="fasvelte1"
 dockerhubserver=""
 dockeruser="haan78"
 dockercluster="baris"
+mode=staging
 
-npmimg="$imageadi/npm"
+npmimg="$imagename/npm"
 #mevcut klasor
 cdir=$(pwd)
-version=$(cat "$cdir/version.txt")
+version=$(cat $cdir/version.txt)
+dockeraccesstoken=$(cat $cdir/config/hidden/docker_hub_token.txt)
 
 #frontendi compile etmek icin npm i olustur
 docker build -f $cdir/DockerNpm -t $npmimg $cdir
@@ -24,9 +26,9 @@ fi
 rm -rf /static/dist
 
 #npm'i run et ve dist klasorunu tekrar olustur
-docker run -v $cdir/static:/app/static:rw --env SCRIPT=staging $npmid $cdir
+docker run -v $cdir/static:/app/static:rw --env SCRIPT=$mode $npmid $cdir
 
-if [ ! -d "/static/dist" ]
+if [ ! -d "$cdir/static/dist" ]
 then
     echo "Dist folder can't create"
     exit 1 
@@ -36,7 +38,7 @@ fi
 docker image rm $npmid
 
 #Uygulama sunucusunu hazirla
-docker build -f $cdir/DockerPy --build-arg MODE=staging --build-arg VERSION=$version -t $imagename $cdir
+docker build -f $cdir/DockerPy --build-arg MODE=$mode --build-arg VERSION=$version -t $imagename $cdir
 
 imgid=$(docker images -q $imagename)
 if [ -z "$imgid" ]
@@ -47,9 +49,9 @@ fi
 
 echo "Image Name = $imagename, Image ID = $imgid"
 
-cat $cdir/config/hidden/docker.hub.txt | docker login -u $dockeruser --password-stdin $dockerhubserver
+echo $dockeraccesstoken | docker login -u $dockeruser --password-stdin $dockerhubserver
 docker image tag $imagename:latest $dockeruser/$dockercluster:$imagename
-dhash=$(docker image push -q $dockeruser/$dockercluster:$image)
+dhash=$(docker image push -q $dockeruser/$dockercluster:$imagename)
 docker logout
 
 if [ -z "$dhash" ]
@@ -58,7 +60,12 @@ then
     exit 1
 fi
 
-echo "Image is ready"
-echo "URL = $dockerhubserver/$dockeruser/$dockercluster:$imagename"
+clear
+echo "Looks good! Deployment steps on application server should be like these"
+echo "Step 1: docker login -u [USERNAME] -p [PASSWORD] $dockerhubserver"
+echo "Step 2: docker pull $dockeruser/$dockercluster:$imagename"
+echo "Step 3: docker logout"
+echo "Step 4: docker run -d -p [LOCAL]:[CONTAINER] $imagename"
+
 exit 0
 
